649a90963fb9775573c9fba1623d1ca824dec5e3
Danilo Cortes <dacortes63@misena.edu.co>
2022-09-14 02:53:17 -0500

    chore: se generan los metodos principales para las operaciones
    se generan los principales metodos para las operaciones y se realizan las validaciones.

--- app/src/main/java/com/ucompensar/calculator/MainActivity.java
+++ app/src/main/java/com/ucompensar/calculator/MainActivity.java
@@ -25,11 +25,19 @@ public class MainActivity extends AppCompatActivity {
     */
     private TextView screen;
     private TextView result;
+    int operation = 0;
+    double firstNumber = 0;
+    double secondNumber = 0;
+    double total = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int operation = 0;
+        double firstNumber = 0;
+        double secondNumber = 0;
+        double total = 0;
 
         screen = (TextView) findViewById(R.id.Screen);
         result = (TextView) findViewById(R.id.Result);
@@ -40,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
 
         CharSequence value = screen.getText();
+
         switch (view.getId()) {
             case R.id.OneButton:
                 int one = 1;
@@ -90,20 +99,123 @@ public class MainActivity extends AppCompatActivity {
                     screen.setText("" + value);
                 }
                 break;
-            case R.id.PlusButton:
+                /*
+            case R.id.PointButton:
+                screen.setText(value +".");
                 double firstNumber = Double.parseDouble(screen.getText().toString());
+                System.out.println(firstNumber);
+                break;
+                 */
+            case R.id.PlusButton:
+                operation = 1;
+                if (firstNumber == 0) {
+                    firstNumber = Double.parseDouble(screen.getText().toString());
+                    screen.setText("");
+                } else {
+                    screen.setText("" + firstNumber);
+                    secondNumber = Double.parseDouble(screen.getText().toString());
+                    screen.setText("");
+                }
+                break;
 
-                screen.setText(value + "+");
+            case R.id.MinosButton:
+                operation = 2;
+                if (firstNumber == 0) {
+                    firstNumber = Double.parseDouble(screen.getText().toString());
+                    screen.setText("");
+                } else {
+                    screen.setText("" + firstNumber);
+                    secondNumber = Double.parseDouble(screen.getText().toString());
+                    screen.setText("");
+                }
+                break;
+            case R.id.TimesButton:
+                operation = 3;
+                if (firstNumber == 0) {
+                    firstNumber = Double.parseDouble(screen.getText().toString());
+                    screen.setText("");
+                } else {
+                    screen.setText("" + firstNumber);
+                    secondNumber = Double.parseDouble(screen.getText().toString());
+                    screen.setText("");
+                }
+                break;
+            case R.id.DividedButton:
+                operation = 4;
+                if (firstNumber == 0) {
+                    firstNumber = Double.parseDouble(screen.getText().toString());
+                    screen.setText("");
+                } else {
+                    screen.setText("" + firstNumber);
+                    secondNumber = Double.parseDouble(screen.getText().toString());
+                    screen.setText("");
+                }
+                break;
+            case R.id.EqualsButton:
+                secondNumber = Double.parseDouble(screen.getText().toString());
+                switch (operation) {
+                    case 1:
+                        total = addittion(firstNumber, secondNumber);
+                        result.setText("" + Math.round(total));
+                        screen.setText("");
+                        break;
+                    case 2:
+                        total = substraction(firstNumber, secondNumber);
+                        result.setText("" + Math.round(total));
+                        screen.setText("");
+                        break;
+                    case 3:
+                        total = multiplication(firstNumber, secondNumber);
+                        result.setText("" + Math.round(total));
+                        screen.setText("");
+                        break;
+                    case 4:
+                        if (secondNumber > 0) {
+                            total = dividedBy(firstNumber, secondNumber);
+                            result.setText("" + Math.round(total));
+                            screen.setText("");
+                        }else {
+                            result.setText("MathError");
+                        }
+                        break;
+                }
+                screen.setText("");
+                operation = 0;
+                firstNumber = total;
+                total = 0;
 
-                result.setText("" + firstNumber);
                 break;
 
 
         }
+
     }
-    public double suma(double firstValue, double secondValue ){
+
+    public void erase() {
+
+    }
+
+    public double addittion(double firstValue, double secondValue) {
         double addittion = firstValue + secondValue;
         return addittion;
+    }
 
+    public double substraction(double firstValue, double secondValue) {
+        double substraction = firstValue - secondValue;
+        return substraction;
+    }
+
+    public double multiplication(double firstValue, double secondValue) {
+        double multiplication = firstValue * secondValue;
+        return multiplication;
+    }
+
+    public double dividedBy(double firstValue, double secondValue) {
+        if (secondValue > 0) {
+            double dividedBy = firstValue / secondValue;
+            return dividedBy;
+        } else {
+            return 0;
+        }
     }
 }
